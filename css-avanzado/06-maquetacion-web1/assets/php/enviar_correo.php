2c75b29309fc0acd912ca42cb85a9f832db5d996
Adrian Peñalver <adrian1414@hotmail.es>
2024-08-24 00:04:43 +0200

    terminando curriculum

---
+++ css-avanzado/06-maquetacion-web1/assets/php/enviar_correo.php
@@ -0,0 +1,33 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Configurar el destinatario del correo (tu email)
+    $to = "adrian1414@hotmail.es";
+
+    // Asunto del correo
+    $to = "Nuevo mensaje del formulario de contacto";
+
+    // Cuerpo del mensaje
+    $cuerpo = "Nombre: " . $name . "\n";
+    $cuerpo .= "Correo electrónico: " . $email . "\n";
+    $cuerpo .= "Mensaje:\n" . $message;
+
+    // Encabezados del correo
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+
+    // Enviar el correo
+    if (mail($to, $asunto, $cuerpo, $headers)) {
+        echo "El correo se ha enviado correctamente.";
+    } else {
+        echo "Hubo un problema al enviar el correo.";
+    }
+}
+
+?>
